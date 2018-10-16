b5aa5b74d8c03f232ba473ce0dd94630da8335db
jsm <sang93423@gmail.com>
2018-10-16 18:28:34 +0900

    s3 bucket에 올리기 위한 코드 추가 collectstatic

---
+++ app/config/storages.py
@@ -0,0 +1,15 @@
+# S3Boto3 Storage로 STATICFILES_STORAGE설정하신분들은
+# 해제하고 ROOT_DIR/.static을 STATIC_ROOT로 사용하도록 수정
+from storages.backends.s3boto3 import S3Boto3Storage
+
+__all__ = (
+    'S3DefaultStorage',
+)
+
+
+class S3StaticStorage(S3Boto3Storage):
+    default_acl = 'public-read'
+    
+    
+class S3DefaultStorage(S3Boto3Storage):
+    default_acl = 'private'
