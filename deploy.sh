8718d669791d5c15a580d80bcf74d6d20591b999
jsm <sang93423@gmail.com>
2018-10-17 12:12:18 +0900

    deploy.sh작성

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# requirements만들기
+pipenv lock --requirements > requirements.txt
+
+# .secrets의 requirements를 staging area에 추가
+git add -f .secrets/ requirements.txt
+
+# eb deloy실행
+eb deploy --profile jsm-eb-user --staged
+
+# .secerets와 requirements를 staging area에서 제거
+git reset HEAD .secrets/ requirements.txt
+
+# requirements.txt삭제
+rm requirements.txt
