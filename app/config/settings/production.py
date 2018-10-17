680968c52eddce81da0e7896b7f18b28287e6a67
jsm <sang93423@gmail.com>
2018-10-17 12:11:58 +0900

    production.py에 runserver로 실행할 경우를 위한 코드 추가

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,11 +1,25 @@
+import sys
+
 from .base import *
 secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
 
+RUNSERVER = 'runserver' in sys.argv
 DEBUG = False
 
 ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
+# runserver로 production환경을 실행할 경우
+if RUNSERVER:
+    DEBUG = True
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+    ]
+
 WSGI_APPLICATION = 'config.wsgi.production.application'
+INSTALLED_APPS += [
+    'storages',
+]
 
 DATABASES = secrets['DATABASES']
 
