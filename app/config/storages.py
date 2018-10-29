5ba2735e447558f944b3e4273e8df9e2f31c8140
jsm <sang93423@gmail.com>
2018-10-29 15:42:23 +0900

    location추가해줌

--- app/config/storages.py
+++ app/config/storages.py
@@ -4,12 +4,15 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 __all__ = (
     'S3DefaultStorage',
+    'S3StaticStorage',
 )
 
 
 class S3StaticStorage(S3Boto3Storage):
     default_acl = 'public-read'
+    location = 'static'
     
     
 class S3DefaultStorage(S3Boto3Storage):
     default_acl = 'private'
+    location = 'media'
